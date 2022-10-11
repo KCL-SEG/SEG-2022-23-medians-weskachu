6a8c7fb81ffa5cc222d259fb8135df99a5bf522e
Wesley Teo <ling.r.teo@kcl.ac.uk>
2022-10-11 20:07:57 +0100

    first attempt

--- median.py
+++ median.py
@@ -9,4 +9,12 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+length = len(numbers)
+
+if length % 2 == 1:
+    print(numbers[length // 2])
+else:
+    middle = length // 2
+    print((numbers[middle - 1] + numbers[middle]) / 2) 
